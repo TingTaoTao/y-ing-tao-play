74ba40eeb2b0b580098387d42af687edea63fe57
贾涛 <wb-jt237903@alibaba-inc.com>
2017-03-20 14:24:22 +0800

    恢复标题栏

--- YingTao/ijkpalyer-view/src/main/java/com/yt/ijkpalyer_view/videoplay/activity/ZZPlayerDemoActivity.java
+++ YingTao/ijkpalyer-view/src/main/java/com/yt/ijkpalyer_view/videoplay/activity/ZZPlayerDemoActivity.java
@@ -24,9 +24,9 @@ public class ZZPlayerDemoActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);//隐藏标题
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);//设置全屏
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);//隐藏标题
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);//设置全屏
         setContentView(R.layout.activity_custom_player_demo);
 
         initData();
